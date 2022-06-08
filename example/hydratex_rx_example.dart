7a0a88c1579bcebcad1de6aa1f9982860598b04f
Luan Emanuel <contato@luanemanuel.com.br>
2022-06-08 14:01:16 -0300

    Package creation

---
+++ example/hydratex_rx_example.dart
@@ -0,0 +1,6 @@
+import 'package:hydratex_rx/hydratex_rx.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/hydratex_rx_test.dart
@@ -0,0 +1,16 @@
+import 'package:hydratex_rx/hydratex_rx.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
